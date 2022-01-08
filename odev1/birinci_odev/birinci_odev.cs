fdc573c6d5d9d4504ea8862fa5a27ca4a9c5486f
Can Taspatlatanlar <can.taspatlatanlar@gmail.com>
2022-01-08 16:14:36 +0300

    Ödev 1 - Algoritma Soruları ilk sorusunun cevabı eklendi.

---
+++ odev1/birinci_odev/birinci_odev.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace birinci_odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Merhaba, lütfen dizinin eleman sayısını giriniz: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if(n > 0)
+            {
+                int[] dizi = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Lütfen {0}. sayıyı giriniz: ", i+1);
+                    dizi[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                for (int x = 0; x < n; x++)
+                {
+                    if(dizi[x] % 2 == 0)
+                    {
+                        Console.WriteLine(dizi[x]);
+                    }
+                }
+            }
+
+        }
+    }
+}
