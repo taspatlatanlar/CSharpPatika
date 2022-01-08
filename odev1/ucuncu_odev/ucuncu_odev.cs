ce2b56d5fef62b106216fc0e985a9c20816aa13d
Can Taspatlatanlar <can.taspatlatanlar@gmail.com>
2022-01-08 17:25:42 +0300

    Ödev 1 - Algoritma Soruları üçüncü sorusunun cevabı eklendi.

---
+++ odev1/ucuncu_odev/ucuncu_odev.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ucuncu_odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Merhaba, lütfen pozitif bir sayı giriniz: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if(n > 0)
+            {
+                string[] dizi = new string[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Lütfen {0}. elemanı giriniz: ", i+1);
+                    dizi[i] = Console.ReadLine();
+                }
+
+                for (int x = n-1; x >= 0; x--)
+                {
+                    Console.WriteLine(dizi[x]);
+                }
+            }
+        }
+    }
+}
