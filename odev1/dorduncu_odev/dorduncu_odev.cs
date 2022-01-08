8786390207eb8a18cab786a7385f8e1552a5f570
Can Taspatlatanlar <can.taspatlatanlar@gmail.com>
2022-01-08 22:19:04 +0300

    Ödev 1 - Algoritma Soruları dördüncü sorusunun cevabı eklendi.

---
+++ odev1/dorduncu_odev/dorduncu_odev.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace dorduncu_odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz: ");
+            string cumle = Console.ReadLine();
+
+            string[] bolunmusCumle = cumle.Split(" ");
+            char[] harfler = cumle.ToCharArray();
+            int kelimeSayisi = 0;
+            int harfSayisi = 0;
+
+            foreach (var kelime in bolunmusCumle)
+            {
+                kelimeSayisi++;
+            }
+
+            foreach (var harf in harfler)
+            {
+                if(harf == ' ' || harf == ',' || harf == '.')
+                {
+                    continue;
+                }
+                harfSayisi++;
+            }
+            Console.WriteLine("Girdiğiniz cümledeki kelime sayısı: " + kelimeSayisi);
+            Console.WriteLine("Girdiğiniz cümledeki harf sayısı: " + harfSayisi);
+        }
+    }
+}
